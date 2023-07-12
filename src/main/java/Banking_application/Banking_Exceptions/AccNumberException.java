f8cf261a7298c6fab82e3fbe3356db88c391d7e7
Rahman <aterahman@deloitte.com>
2023-07-12 20:50:57 +0530

    Created custom exception to validate Account Number

--- src/main/java/Banking_application/Banking_Exceptions/AccNumberException.java
+++ src/main/java/Banking_application/Banking_Exceptions/AccNumberException.java
@@ -1,2 +1,9 @@
-package Banking_application.Banking_Exceptions;public class AccNumberException {
+package Banking_application.Banking_Exceptions;
+
+public class AccNumberException extends Exception
+{
+    public  AccNumberException(String errorMessage)
+    {
+        super(errorMessage);
+    }
 }
