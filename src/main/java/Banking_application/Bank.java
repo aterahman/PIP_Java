c1be349af310eadeac54a5b49e9d8b797b0f6db0
aterahman <aterahman@deloitte.com>
2023-07-12 21:47:42 +0530

    Finished account creation

--- src/main/java/Banking_application/Bank.java
+++ src/main/java/Banking_application/Bank.java
@@ -1,2 +1,31 @@
-package Banking_application;public class bank {
+package Banking_application;
+import java.util.Scanner;
+
+public class Bank
+{
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int choice=0;
+        Account_Creation accountCreation = new Account_Creation();
+
+        do {
+            System.out.println("----------------------********************---------------------------");
+            System.out.println("Hello! Welcome to the Banking Application! What would you like to do?");
+            System.out.println("1. Create Bank Account\n2. Check Existing Account Details\n3. Withdraw Amount\n4. Deposit Amount\n5. Delete Account\n6. Exit");
+            System.out.println("----------------------********************---------------------------");
+            System.out.println("Please Enter Your Choice!");
+            choice = sc.nextInt();
+
+            switch (choice)
+            {
+                case 1 : accountCreation.Acc_Creation();
+
+            }
+
+        }
+        while (choice!=0);
+
+
+    }
 }
