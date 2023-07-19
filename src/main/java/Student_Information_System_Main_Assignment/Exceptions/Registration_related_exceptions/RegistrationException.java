c261524e08df46d354fe6f5aced549449036141c
aterahman <aterahman@deloitte.com>
2023-07-19 11:53:00 +0530

    Custom exception for registration related issues

--- src/main/java/Student_Information_System_Main_Assignment/Exceptions/Registration_related_exceptions/RegistrationException.java
+++ src/main/java/Student_Information_System_Main_Assignment/Exceptions/Registration_related_exceptions/RegistrationException.java
@@ -1,2 +1,10 @@
-package Student_Information_System_Main_Assignment.Exceptions.Registration_related_exceptions;public class RegistrationException {
+package Student_Information_System_Main_Assignment.Exceptions.Registration_related_exceptions;
+
+public class RegistrationException extends Exception
+{
+    public RegistrationException(String errorMessage)
+    {
+        super(errorMessage);
+    }
+
 }
