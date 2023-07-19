1db94b212f4e6e6a81e9e70a16b6795b436a0ca3
aterahman <aterahman@deloitte.com>
2023-07-19 11:50:32 +0530

    Added and linked with class that will update the etails based on user input

--- src/main/java/Student_Information_System_Main_Assignment/User_interface/User_Interface_Screen.java
+++ src/main/java/Student_Information_System_Main_Assignment/User_interface/User_Interface_Screen.java
@@ -7,6 +7,7 @@ import java.util.Scanner;
 
 public class User_Interface_Screen {
 
+    //method to enter roll number and check if it exists, method to promote code re-usability
     public String Enter_Roll_No() throws IOException {
         Scanner sc = new Scanner(System.in);
         Validate_roll_no_existence validateRollNoExistence = new Validate_roll_no_existence();
@@ -32,6 +33,7 @@ public class User_Interface_Screen {
         Scanner sc = new Scanner(System.in);
         Add_Student_Details addStudentDetails = new Add_Student_Details();
         Search_Student_Details searchStudentDetails = new Search_Student_Details();
+        Update_Student_Details updateStudentDetails = new Update_Student_Details();
         Delete_Student_Details deleteStudentDetails = new Delete_Student_Details();
 
         int choice;
@@ -41,6 +43,7 @@ public class User_Interface_Screen {
         System.out.println("Welcome User! What would you like to do today?");
         System.out.println("1. Add Student Details\n2. Search Student Details\n3. Update Student Details\n4. Delete Student Details\n5. Logout");
         System.out.println("-------------------------------------------------------");
+        System.out.println("Please enter choice");
 
         //entering choice
          choice = sc.nextInt();
@@ -48,23 +51,51 @@ public class User_Interface_Screen {
 
             switch (choice) {
                 case 1 -> {
+                    //calling class that will add data
                     addStudentDetails.Add_details();
                 }
-                case 2 -> {
 
+                //searching data
+                case 2 -> {
+                    //taking roll no. input and checking if roll no. exists
                    String roll_number = Enter_Roll_No();
+
+                   //calling class that will search for the data
                    searchStudentDetails.Search_Details(roll_number);
                 }
+
+                //updating data
                 case 3 ->{
+                    //taking roll no. input and checking if roll no. exists
                     String roll_number = Enter_Roll_No();
 
+                    //getting sata already present
+                    searchStudentDetails.Search_Details(roll_number);
+
+                    //sending the store data
+                   updateStudentDetails.user_prompt(roll_number,searchStudentDetails.searched_name,searchStudentDetails.searched_age,searchStudentDetails.searched_course,searchStudentDetails.searche_grade);
+
+                   //displaying updated data
+                    System.out.println("Updated data is : ");
+                    searchStudentDetails.Search_Details(roll_number);
+
                 }
+
+                //deleting data
                 case 4 -> {
+                    //taking roll no. input and checking if roll no. exists
                     String roll_number = Enter_Roll_No();
+
+                    //prompt for user to confirm decision
                     System.out.println("Are you sure you want to delete?(Y/N)");
                     char ch = sc.next().charAt(0);
+
                     switch (Character.toUpperCase(ch)) {
+
+                        //calling class that will delete data if yes
                         case 'Y' -> deleteStudentDetails.Delete_Student_Details(roll_number);
+
+                        //returning to user interface screen if no
                         case 'N' -> {
                             System.out.println("No data deleted!");
                             User_Interface_Screen();
@@ -75,6 +106,8 @@ public class User_Interface_Screen {
                         }
                     }
                 }
+
+                //exiting app
                 case 5 ->
                 {
                     System.out.println("Goodbye!");
