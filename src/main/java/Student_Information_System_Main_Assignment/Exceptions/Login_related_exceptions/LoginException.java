f13b848c290c991440e7f698035b6f065effb18e
aterahman <aterahman@deloitte.com>
2023-07-19 11:51:35 +0530

    Custom exception for login related issues

--- src/main/java/Student_Information_System_Main_Assignment/Exceptions/Login_related_exceptions/LoginException.java
+++ src/main/java/Student_Information_System_Main_Assignment/Exceptions/Login_related_exceptions/LoginException.java
@@ -1,2 +1,9 @@
-package Student_Information_System_Main_Assignment.Exceptions.Login_related_exceptions;public class LoginException {
+package Student_Information_System_Main_Assignment.Exceptions.Login_related_exceptions;
+
+public class LoginException extends Exception
+{
+    public LoginException(String errorMessage)
+    {
+        super(errorMessage);
+    }
 }
