02257bd280be5b6a7ba6c9add1c0d273ad211b50
aterahman <aterahman@deloitte.com>
2023-07-19 11:55:52 +0530

    added code to validate if the roll number already exists

--- src/main/java/Student_Information_System_Main_Assignment/Exceptions/Student_details_related/Validate_Added_Roll_Number.java
+++ src/main/java/Student_Information_System_Main_Assignment/Exceptions/Student_details_related/Validate_Added_Roll_Number.java
@@ -1,2 +1,36 @@
-package Student_Information_System_Main_Assignment.Exceptions.Student_details_related;public class Validate_Roll_Number {
+package Student_Information_System_Main_Assignment.Exceptions.Student_details_related;
+
+import Student_Information_System_Main_Assignment.Exceptions.Registration_related_exceptions.RegistrationException;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class Validate_Added_Roll_Number
+{
+    public void validate_added_roll_no(String roll_no)throws RollNumberException, IOException
+    {
+        File acc_data_csv = new File("src\\main\\resources\\Student_Information_System_Resources\\Student_Details.csv");
+
+        //using a reader that will read from file
+        Reader file_read = new FileReader(acc_data_csv);
+
+        //parsing the data that has been read
+        CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(file_read);
+
+
+        //CHECKING IF ACCOUNT NUMBER ALREADY EXISTS
+        for (CSVRecord record : parser) {
+            String roll_no_in_csv = record.get(0);
+
+            if (roll_no_in_csv.equals(roll_no)) {
+                throw new RollNumberException("Roll No. already exists!");
+            }
+
+        }
+    }
 }
