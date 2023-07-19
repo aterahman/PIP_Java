309e98e657ce46b2db4ffb0e02d9a29ba45cb521
aterahman <aterahman@deloitte.com>
2023-07-19 10:41:32 +0530

    created class to search for student data from the csv file

--- src/main/java/Student_Information_System_Main_Assignment/User_interface/Search_Student_Details.java
+++ src/main/java/Student_Information_System_Main_Assignment/User_interface/Search_Student_Details.java
@@ -1,2 +1,41 @@
-package Student_Information_System_Main_Assignment.User_interface;public class Search_Student_Details {
+package Student_Information_System_Main_Assignment.User_interface;
+
+import Student_Information_System_Main_Assignment.Exceptions.Student_details_related.RollNumberException;
+import Student_Information_System_Main_Assignment.Exceptions.Student_details_related.Validate_roll_no_existence;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Search_Student_Details
+{
+    public void Search_Details(String roll_no) throws IOException {
+        File acc_data_csv = new File("src\\main\\resources\\Student_Information_System_Resources\\Student_Details.csv");
+
+        //using a reader that will read from file
+        Reader file_read = new FileReader(acc_data_csv);
+
+        //parsing the data that has been read
+        CSVParser parser = CSVFormat.DEFAULT.withDelimiter(',').withHeader().parse(file_read);
+
+
+        //CHECKING IF ACCOUNT NUMBER ALREADY EXISTS
+        for (CSVRecord record : parser) {
+
+            String roll_no_in_csv= record.get(0);
+            String name = record.get(1);
+            String age = record.get(2);
+            String course = record.get(3);
+            String grade = record.get(4);
+
+            //displaying the student details
+            if (roll_no_in_csv.equals(roll_no)) {
+
+                System.out.println("Name | Roll No. | Age | Course | Grade");
+                System.out.println(name+" | "+roll_no_in_csv+" | "+age+" | "+course+" | "+grade);
+            }
+        }
+    }
 }
