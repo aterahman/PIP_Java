72916111d306bbf09d07b876b2cdd265df1a09a7
aterahman <aterahman@deloitte.com>
2023-07-19 11:54:57 +0530

    created custom exception for roll number related issues

--- src/main/java/Student_Information_System_Main_Assignment/Exceptions/Student_details_related/RollNumberException.java
+++ src/main/java/Student_Information_System_Main_Assignment/Exceptions/Student_details_related/RollNumberException.java
@@ -1,2 +1,9 @@
-package Student_Information_System_Main_Assignment.Exceptions.Student_details_related;public class RollNumberException {
+package Student_Information_System_Main_Assignment.Exceptions.Student_details_related;
+
+public class RollNumberException extends Exception
+{
+    public RollNumberException(String errorMessage)
+    {
+        super(errorMessage);
+    }
 }
